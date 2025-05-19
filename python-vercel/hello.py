9cdde8387cb69bb6c6ff82ad29a01d5b839d42b0
ᑌs๓ᴀñ̰︵khokhar <bydjusman@gmail.com>
2025-05-19 16:01:59 +0500

    first commit

--- python-vercel/hello.py
+++
@@ -1,7 +0,0 @@
-def handler(request):
-    name = request.get("query", {}).get("name", "World")
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": f'{{"message": "Hello, {name}!"}}',
-    }
